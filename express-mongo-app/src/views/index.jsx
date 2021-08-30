6e59695723954e44e7be447b11abd578bc55e51f
haithamshahin333 <42976403+haithamshahin333@users.noreply.github.com>
2021-08-30 10:45:50 -0400

    Update index.jsx

--- express-mongo-app/src/views/index.jsx
+++ express-mongo-app/src/views/index.jsx
@@ -9,11 +9,11 @@ function DataEntryForm() {
                         type="text"
                         name="name"
                         id="name"
-                        placeholder="name"
+                        placeholder="pipeline demo - name"
                     />
                 </div>
                 <div>
-                    <input type="text" name="job" id="job" placeholder="job" />
+                    <input type="text" name="job" id="job" placeholder="pipeline demo - job" />
                 </div>
                 <input type="submit" />
             </form>
